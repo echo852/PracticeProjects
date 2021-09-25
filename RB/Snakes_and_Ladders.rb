076847b444eaac5bed21bb52feb5eb523c93089c
EMcRae <echo852@gmail.com>
2021-09-25 09:17:00 -0700

    need to change scope in variables duh

--- RB/Snakes_and_Ladders.rb
+++ RB/Snakes_and_Ladders.rb
@@ -4,33 +4,33 @@
 # NOT COMPLETE YET
 
 class SnakesLadders
-    snake_squares = [[16,6],[46,25],[49,11],[62,19],[64,60],[74,53],[89,68],[92,88],[95,75],[99,80]]
-    ladder_squares = [[2,38],[7,14],[8,31],[15,26],[21,42],[28,84],[36,44],[51,67],[71,91],[78,98],[87,94]]
+    @@snake_squares = [[16,6],[46,25],[49,11],[62,19],[64,60],[74,53],[89,68],[92,88],[95,75],[99,80]]
+    @@ladder_squares = [[2,38],[7,14],[8,31],[15,26],[21,42],[28,84],[36,44],[51,67],[71,91],[78,98],[87,94]]
 
-    # whose turn is it?
-    player_number = 1
+    # whose turn is it? it'll have to be 0 and 1 instead of 1 and 2 for array locations
+    @@player_number = 0
 
     # where are the players currently?
-    player_locations = [[1,0],[2,0]]
+    @@player_locations = [0,0]
 
     # check to see if the player landed on a snake or a ladder
     def square_check(location)
-        snake_end = snake_squares.select {|(head,tail)|
+        snake_end = @@snake_squares.select {|(head,tail)|
             head == location
         }.map {|(head,tail)|
             tail
         }
 
-        ladder_end = ladder_squares.select {|(bottom,top)|
+        ladder_end = @@ladder_squares.select {|(bottom,top)|
             bottom == location
         }.map {|(bottom,top)|
             top
         }
 
-        if snake_end != nil
+        if snake_end.join.to_i != nil
             return snake_end
-        elsif ladder_end != nil
-            return ladder_end            
+        elsif ladder_end.join.to_i != nil
+            return ladder_end           
         else
             return 0
         end
@@ -38,43 +38,53 @@ class SnakesLadders
 
     # change current player
     def swap_players
-        if player_number == 1
-            player_number = 2
+        if @@player_number == 0
+            @@player_number = 1
         else
-            player_number = 1
+            @@player_number = 0
         end
     end
 
     # update the current player's location
     def update_location(die_sum)
-        player_locations[player_number] += die_sum
-        current_location = player_locations[player_number]
+        current_location = @@player_locations[@@player_number].to_i
+        new_location = current_location + die_sum
+        @@player_locations[@@player_number] = new_location
         snake_or_ladder_movement = square_check(current_location) # a variable to store the result of checking if the player moved along a snake or a ladder
         if snake_or_ladder_movement != 0
-            player_locations[player_number] = snake_or_ladder_movement
+            @@player_locations[@@player_number] = snake_or_ladder_movement
         else
-            player_locations[player_number] = current_location
+            @@player_locations[@@player_number] = current_location
         end
     end
 
     # check to see where the player is
-    def check_location
-        loc = player_locations[player_number]
+    def check_location(loc)
         if loc == 100
-            return "Congratulations!  Player #{player_number} wins!"
+            return "Congratulations!  Player #{@@player_number} wins!"
         elsif loc > 100
-            # do something... bounce off 100 and go back
+            difference = loc - 100
+            go_back = 100 - difference
+            @@player_locations[@@player_number] = go_back
+            return "Player #{@@player_number} is on square #{go_back}"
+        else
+            return "Player #{@@player_number} is on square #{loc}"
         end
     end
 
     # final method to actually play the game!
     def play(die1,die2)
         sum = die1 + die2
-        # update player's current location
-        # check player's current location
-        # compare die1 and die2
-        # roll again if they're the same
-        # swap to the other player
+        update_location(sum)
+        loc = @@player_locations[@@player_number].join.to_i
+        check_location(loc)
+        if die1 != die2
+            swap_players()
+        end
         # continue until someone reaches exactly 100
     end
 end
+
+game = SnakesLadders.new
+
+game.play(1,1)
